421943dcfe2ad562c12be4b3afd256b75ce622e3
Aleksander Gajewski <adiog@brainfuck.pl>
2018-09-19 13:02:05 +0200

    extract protocol

--- include/DriverGamenastixHelper.h
+++ include/DriverGamenastixHelper.h
@@ -8,50 +8,10 @@
 #include <unordered_map>
 #include <vector>
 
+#include <Protocol.h>
 #include <deps/treadmill-t2-biomodel/include/deepModel/Model.h>
-#include <BodyPart.h>
 
 
-using Message = std::vector<uint8_t>;
-
-enum class MessageType
-{
-    CalibrationNPose,
-    CalibrationSPose,
-    RequestModel,
-    ResponseModel,
-    Frame,
-    Unknown
-};
-
-struct Matcher
-{
-    static MessageType match(Message message)
-    {
-        if (message.size() < 1)
-        {
-            return MessageType::Unknown;
-        }
-        else
-        {
-            switch (message[0])
-            {
-                case 'N':
-                    return MessageType::CalibrationNPose;
-                case 'S':
-                    return MessageType::CalibrationSPose;
-                case 'R':
-                    return MessageType::RequestModel;
-                case 'M':
-                    return MessageType::ResponseModel;
-                case 'F':
-                    return MessageType::Frame;
-                default:
-                    return MessageType::Unknown;
-            }
-        }
-    }
-};
 
 class DriverGamenastix
 {
---
+++ include/Protocol.h
@@ -0,0 +1,51 @@
+// This file is a part of Treadmill project.
+// Copyright 2018 Disco WTMH S.A.
+
+#pragma once
+
+#include <cstdint>
+#include <functional>
+#include <unordered_map>
+#include <vector>
+
+
+using Message = std::vector<uint8_t>;
+
+enum class MessageType
+{
+    CalibrationNPose,
+    CalibrationSPose,
+    RequestModel,
+    ResponseModel,
+    Frame,
+    Unknown
+};
+
+struct Matcher
+{
+    static MessageType match(Message message)
+    {
+        if (message.size() < 1)
+        {
+            return MessageType::Unknown;
+        }
+        else
+        {
+            switch (message[0])
+            {
+                case 'N':
+                    return MessageType::CalibrationNPose;
+                case 'S':
+                    return MessageType::CalibrationSPose;
+                case 'R':
+                    return MessageType::RequestModel;
+                case 'M':
+                    return MessageType::ResponseModel;
+                case 'F':
+                    return MessageType::Frame;
+                default:
+                    return MessageType::Unknown;
+            }
+        }
+    }
+};
